d4f96e3bf77ea7c4d563c2bb33b20ad4d058bfa2
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-06-27 21:48:13 +0300

    HeaderInitialization DONE

--- String/main.cpp
+++ String/main.cpp
@@ -25,30 +25,30 @@ public:
 		return str;
 	}
 
-	explicit String(int size = 80)
+	explicit String(int size = 80) :size(size), str(new char[size] {})
 	{
-		this->size = size;
-		this->str = new char[size] {};
+		//this->size = size;
+		//this->str = new char[size] {};
 		cout << "DefaultConstructor:" << this << endl;
 	}
-	String(const char str[])
+	String(const char str[]) :size(strlen(str) + 1), str(new char[size] {})
 	{
-		this->size = strlen(str) + 1;
-		this->str = new char[size] {};
+		//this->size = strlen(str) + 1;
+		//this->str = new char[size] {};
 		strcpy(this->str, str);
 		cout << "Constructor:\t" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other) :size(other.size), str(new char[size] {})
 	{
-		this->size = other.size;
-		this->str = new char[size] {};
+		//this->size = other.size;
+		//this->str = new char[size] {};
 		for (int i = 0; i < size; i++) this->str[i] = other.str[i];
 		cout << "CopyConstructor:" << this << endl;
 	}
-	String(String&& other)noexcept
+	String(String&& other)noexcept :size(other.size), str(other.str)
 	{
-		this->size = other.size;
-		this->str = other.str;
+		//this->size = other.size;
+		//this->str = other.str;
 
 		other.size = 0;
 		other.str = nullptr;
@@ -127,6 +127,7 @@ std::ostream& operator<<(std::ostream& os, const String& str)
 //#define CONSTRUCTORS_CHECK
 //#define OPERATOR_PLUS_CHECK
 #define MOVE_ASSIGNMENT_CHECK
+//#define CALLING_CONSTRUCTORS
 
 void main()
 {
@@ -188,4 +189,29 @@ void main()
 	cout << str4 << endl;
 	cout << delimiter << endl;
 #endif // MOVE_ASSIGNMENT_CHECK
+
+#ifdef CALLING_CONSTRUCTORS
+	String str1;
+	str1.print();
+
+	//String str2 = 8;	//explisit constructor так вызвать нельзя
+	String str2(8);		//explisit constructor можно вызвать только так
+	str2.print();
+
+	String str3 = "Hello";	//Single-Argument constructor 'char'
+	str3.print();
+
+	String str4();		//не вызывается никакой конструктор, и не создается объект, здесь объявляется функция str4(), которая не принимает никаких параметров
+						//которая не принимает никаких параметров и возвращает значение типа 'String'
+	//str4.print();
+
+	String str5{};		//явный вызов конструктора по умолчанию
+	str5.print();
+
+	//String str6 = str3;	//CopyConstructor
+	//String str6(str3);	//CopyConstructor
+	String str6{ str3 };	//CopyConstructor
+	str6.print();
+#endif // CALLING_CONSTRUCTORS
+
 }
